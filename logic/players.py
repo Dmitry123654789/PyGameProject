57db4bad8d6b8c2c071f45b39913b02ec4d5d6b8
Dmitry <pokemon2708@yandex.ru>
2024-12-20 21:51:00 +0300

    Добавлена новые клавиши для ходьбы

--- logic/players.py
+++ logic/players.py
@@ -1,7 +1,9 @@
+import pygame as pg
+
 from logic.seting import *
 
 
-class Player(pygame.sprite.Sprite):
+class Player(pg.sprite.Sprite):
     image = load_image("images/dog_sprites.png")
 
     def __init__(self, x, y, *group):
@@ -9,42 +11,55 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.up_sprite = split_image_to_surfaces(self.image, 32, CELL_SIZE, 11, 3, 5)
-        self.down_sprite = split_image_to_surfaces(self.image, 32, CELL_SIZE, 11, 0, 5)
-        self.right_sprite = split_image_to_surfaces(self.image, 32, CELL_SIZE, 11, 2, 5)
-        self.left_sprite = split_image_to_surfaces(self.image, 32, CELL_SIZE, 11, 1, 5)
-        self.lst_start_img = [self.up_sprite[1], self.down_sprite[1], self.right_sprite[1], self.left_sprite[1]]
+        self.lst_sprites = []
+        for i in range(8):
+            self.lst_sprites.append(split_image_to_surfaces(self.image, 32, CELL_SIZE, 11, i, 5))
+
         self.ind_sprite = 0
-        self.image = self.right_sprite[-1]
         self.animation_timer = 0
         self.animation_interval = 50
         self.step = 8
-        self.direction = 0
+        self.dict_direction = {0: (0, 1), 1: (-1, 0), 2: (1, 0), 3: (0, -1), 4: (-1, 1), 5: (-1, -1), 6: (1, 1), 7: (1, -1)}
+        self.direction = 2 # down, left, right, up, left_down, left_up, right_down, right_up
+        self.go = False
+        self.dict_key = {'down': (pg.K_DOWN, pg.K_s), 'up': (pg.K_UP, pg.K_w), 'right': (pg.K_RIGHT, pg.K_d), 'left': (pg.K_LEFT, pg.K_a)}
+
+    def side(self, key, side):
+        return any(key[i] for i in self.dict_key[side])
 
     def input(self):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_UP]:
-            self.image = self.up_sprite[self.ind_sprite]
-            self.rect.y -= self.step
-            self.direction = 0
-        elif key[pygame.K_DOWN]:
-            self.image = self.down_sprite[self.ind_sprite]
-            self.rect.y += self.step
-            self.direction = 1
-        elif key[pygame.K_RIGHT]:
-            self.image = self.right_sprite[self.ind_sprite]
-            self.rect.x += self.step
-            self.direction = 2
-        elif key[pygame.K_LEFT]:
-            self.image = self.left_sprite[self.ind_sprite]
-            self.rect.x -= self.step
+        self.go = True
+        key = pg.key.get_pressed()
+        if self.side(key, 'up') and self.side(key, 'right'):
+            self.direction = 7
+        elif self.side(key, 'up') and self.side(key, 'left'):
+            self.direction = 5
+        elif self.side(key, 'down') and self.side(key, 'left'):
+            self.direction = 4
+        elif self.side(key, 'down') and self.side(key, 'right'):
+            self.direction = 6
+        elif self.side(key, 'up'):
             self.direction = 3
+        elif self.side(key, 'down'):
+            self.direction = 0
+        elif self.side(key, 'right'):
+            self.direction = 2
+        elif self.side(key, 'left'):
+            self.direction = 1
         else:
-            self.image = self.lst_start_img[self.direction]
+            self.go = False
+            self.image = self.lst_sprites[self.direction][1]
+
+        if self.go:
+            self.image = self.lst_sprites[self.direction][self.ind_sprite]
+            self.rect.x += self.step * self.dict_direction[self.direction][0]
+            self.rect.y += self.step * self.dict_direction[self.direction][1]
 
     def update(self, tick):
         if tick - self.animation_timer >= self.animation_interval:
             self.ind_sprite += 1
-            self.ind_sprite %= len(self.right_sprite)
+            self.ind_sprite %= len(self.lst_sprites[0])
             self.animation_timer = tick
             self.input()
+
+
