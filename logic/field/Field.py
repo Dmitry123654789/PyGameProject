823f1ce2cfc56b49ab33008b6ad3a0e8edb7b0aa
Dmitry <pokemon2708@yandex.ru>
2025-01-18 22:09:28 +0300

    .

--- logic/field/Field.py
+++ logic/field/Field.py
@@ -1,7 +1,7 @@
 from logic.Entity.Enemy import Enemy
 from logic.Entity.Players import Player
 from logic.seting import *
-from logic.Field.Tiles import *
+from logic.field.Tiles import *
 
 
 class Field(pygame.sprite.Group):
