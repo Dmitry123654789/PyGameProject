8a7cd94a354c898497597c8624da2b79ce2fae75
Dmitry <pokemon2708@yandex.ru>
2025-01-23 15:04:50 +0300

    all del

--- data/Texture_tsx/Snow_tree_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Snow_tree_1" tilewidth="16" tileheight="16" tilecount="112" columns="14">
- <image source="../Texture/Snow_tree_1.png" width="224" height="128"/>
-</tileset>
--- data/Texture_tsx/stolb.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="stolb" tilewidth="22" tileheight="37" tilecount="1" columns="1">
- <image source="../Texture/stolb.png" width="22" height="37"/>
-</tileset>
